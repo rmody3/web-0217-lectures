b1e59b788be042d1ba12481fe4d0ef9ec8a43ece
Jeff Katz <jeffreyerickatz@gmail.com>
2017-02-27 13:21:02 -0500

    move domain modeling

--- restaurant-app/lib/item.rb
+++ restaurant-app/lib/item.rb
@@ -15,39 +15,3 @@ class Item
 
 
 end
-
-
-
-
-# 1. think about belongs to and has many
-# 2. think about instances
-# restaurant
-  # has many items
-# Menu Item
-
-  # Pizza | price
-
-  # Popcorn
-# Order
-#   customer | menu item |  bill
-#   sally        pizza    first_bill
-#   sally      coke    first_bill
-#
-#
-# # Bill           date      waiter
-#   first_bill | saturday | mr. waiter
-#   second_bill | sunday    mr. waiter
-
-#   waiter
-#     name | restaurant
-#     bob     famiglia
-#     fred    famiglia
-      sam   | pizza hut
-#   restaurant
-#     famiglia
-#   name
-
-
-# 1. If it's a belongs to relationship -> the has to live on the model with the belongs_to
-# 2. If it's a has many, we cannot store the data on that item
-# 3.
